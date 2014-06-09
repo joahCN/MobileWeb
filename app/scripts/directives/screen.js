b73874c18fb338d2c5c6bf8f563d176c0880b939
iissnan <iissnan@gmail.com>
2014-06-09 13:11:02 +0800

    Update Directive - Screen. A workaround for transclude scope.

--- app/scripts/directives/screen.js
+++ app/scripts/directives/screen.js
@@ -4,6 +4,15 @@ angular.module("RCM.directives")
             restrict: "E",
             transclude: true,
             replace: true,
-            template: '<div class="screen" ng-transclude></div>'
+            scope: true,
+            template: '<div class="screen"></div>',
+            link: function (scope, element, attrs, ctrl, transcludeFn) {
+
+                // transclude option create a sibling scope of the directive.
+                // http://stackoverflow.com/questions/22575424/angularjs-ng-controller-on-directive-does-not-work-on-transcluded-elements-withi
+                transcludeFn(scope, function(clone) {
+                    element.append(clone);
+                });
+            }
         };
     });
